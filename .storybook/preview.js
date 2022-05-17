eb69b1ab2424074ca5c5d487849bee26c0291582
Bayu Kurnia <hi@bayukurnia.com>
2022-05-18 06:58:01 +0700

    add user theme detection

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,37 @@
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+      date: /Date$/
+    }
+  }
 }
+
+const themeListener = (Story, context) => {
+  function setTheme(newTheme) {
+    document.body.className = newTheme
+    window.__theme = newTheme
+    window.__onThemeChange(newTheme)
+  }
+  window.__onThemeChange = function () {}
+  window.__setPreferredTheme = function (newTheme) {
+    setTheme(newTheme)
+    try {
+      localStorage.setItem('theme', JSON.stringify(window.__theme))
+    } catch (err) {}
+  }
+  const darkQuery = window.matchMedia('(prefers-color-scheme: dark)')
+  darkQuery.addListener(function (event) {
+    window.__setPreferredTheme(event.matches ? 'dark' : 'light')
+  })
+  let preferredTheme
+  try {
+    preferredTheme = JSON.parse(localStorage.getItem('theme'))
+  } catch (err) {}
+  setTheme(preferredTheme || (darkQuery.matches ? 'dark' : 'light'))
+
+  return <Story {...context} />
+}
+
+export const decorators = [themeListener]
