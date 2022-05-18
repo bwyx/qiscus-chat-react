9339fd20d2bcc36cd93b56a704e699051b2f3435
Bayu Kurnia <hi@bayukurnia.com>
2022-05-18 09:09:45 +0700

    persist state

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,16 @@
 import create from 'zustand'
+import { persist } from 'zustand/middleware'
 
-export const useStore = create(() => ({
-  isAuthenticated: false
-}))
+export const useStore = create(
+  persist(
+    () => ({
+      isAuthenticated: false
+    }),
+    {
+      name: 'user'
+    }
+  )
+)
 
 export const setAuthenticated = (isAuthenticated: boolean) =>
   useStore.setState({ isAuthenticated })
