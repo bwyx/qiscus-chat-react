be7ac4e1d9fd11b3fe98eb53219bfeb00d362a50
Bayu Kurnia <hi@bayukurnia.com>
2022-05-18 08:29:04 +0700

    wip routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-import ThemeSwitcher from '~/components/ThemeSwitcher'
+import { Suspense, lazy } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
 
 import globalStyle from '~/styles/global.style'
+
+const Login = lazy(() => import('~/views/Login'))
+const Chat = lazy(() => import('~/views/Chat'))
 
 function App() {
   globalStyle()
 
   return (
     <div className="App">
-      <ThemeSwitcher />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<>...</>}>
+              <Login />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/chat"
+          element={
+            <Suspense fallback={<>...</>}>
+              <Chat />
+            </Suspense>
+          }
+        />
+      </Routes>
     </div>
   )
 }
