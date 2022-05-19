e20d0d1e6d90638c2a01848c50406f90080acd20
Bayu Kurnia <hi@bayukurnia.com>
2022-05-19 13:59:19 +0700

    env config

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_QISCUS_APPID: string
+  readonly VITE_QISCUS_USERID: string
+  readonly VITE_QISCUS_USERNAME: string
+  readonly VITE_QISCUS_PASSKEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
