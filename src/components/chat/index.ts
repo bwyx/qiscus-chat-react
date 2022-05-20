61e59f996c4414a7bc298eb238778bcd2806cd03
Bayu Kurnia <hi@bayukurnia.com>
2022-05-20 07:24:02 +0700

    wip chat room

--- src/components/chat/index.ts
+++ src/components/chat/index.ts
@@ -1,1 +1,2 @@
 export { default as TextBubble } from './TextBubble'
+export { default as ChatInputForm } from './ChatInputForm'
--- src/views/Chat.view.tsx
+++ src/views/Chat.view.tsx
@@ -1,47 +1,99 @@
-import { useState, useEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import useQiscus from '~/hooks/useQiscus'
 
-import { TextBubble } from '~/components/chat'
+import { TextBubble, ChatInputForm } from '~/components/chat'
 import { css } from '~/styles'
 
 const styles = {
   messagesContainer: css({
     mx: '$2',
+    pb: '$20',
     display: 'flex',
     flexDirection: 'column',
+    flexGrow: 1,
     li: {
       mb: '$2'
+    },
+    '@sm': {
+      pb: '$24'
     }
   })()
 }
 
+interface Message {
+  text: string
+  received: boolean
+}
+
 const Chat = () => {
-  const { isReady, qiscus } = useQiscus()
+  const messagesContainer = useRef<HTMLUListElement>(null)
+  const { isReady, qiscus, user } = useQiscus()
   const [queries] = useSearchParams()
   const roomId = queries.get('room')
 
-  const [messages, setMessages] = useState([])
+  const [messages, setMessages] = useState<Message[]>([])
+
+  const mapMessage = (m: any): Message => ({
+    text: m.message,
+    received: m.email !== user.email
+  })
 
   useEffect(() => {
     if (!isReady) return
+    let shouldChangeState = true
 
-    qiscus.getRoomById(roomId).then((resp: any) => setMessages(resp.comments))
+    qiscus.getRoomById(roomId).then(({ comments }: any) => {
+      const messagesHistory: Message[] = comments.map(mapMessage)
+
+      if (!shouldChangeState) return
+      setMessages(messagesHistory)
+    })
+
+    return () => {
+      shouldChangeState = false
+    }
   }, [isReady, roomId])
 
+  useEffect(() => {
+    messagesContainer.current?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'end'
+    })
+  }, [messages])
+
+  useEffect(() => {
+    let shouldChangeState = true
+    qiscus.options.newMessagesCallback = (m: any) => {
+      const thisRoomMessage = m.filter((m: any) => m.room_id_str === roomId)
+
+      if (thisRoomMessage.length === 0) return
+      const newMessages: Message[] = m.map(mapMessage)
+
+      if (!shouldChangeState) return
+      setMessages((messages) => [...messages, ...newMessages])
+    }
+
+    return () => {
+      shouldChangeState = false
+      qiscus.options.newMessageCallback = null
+    }
+  }, [])
+
   return (
-    <div>
+    <>
       <h1>Chat</h1>
-      <ul className={styles.messagesContainer}>
-        {messages.map(({ message, isSent }: any, i: number) => {
+      <ul ref={messagesContainer} className={styles.messagesContainer}>
+        {messages.map((props: Message, i: number) => {
           return (
             <li key={i}>
-              <TextBubble received={!isSent} text={message} />
+              <TextBubble {...props} />
             </li>
           )
         })}
       </ul>
-    </div>
+      <ChatInputForm roomId={roomId} />
+    </>
   )
 }
 
